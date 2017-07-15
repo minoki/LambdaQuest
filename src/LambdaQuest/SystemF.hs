d16a8b3e40583f1224d2bddc1272e395c8e1dec9
ARATA Mizuki <minorinoki@gmail.com>
2017-07-15 13:49:11 +0900

    Add LambdaQuest.SystemF

---
+++ src/LambdaQuest/SystemF.hs
@@ -0,0 +1,8 @@
+module LambdaQuest.SystemF
+  (module LambdaQuest.SystemF.Type
+  ,module LambdaQuest.SystemF.PrettyPrint
+  ,parseType,parseTerm
+  ) where
+import LambdaQuest.SystemF.Type
+import LambdaQuest.SystemF.PrettyPrint
+import LambdaQuest.SystemF.Parse
